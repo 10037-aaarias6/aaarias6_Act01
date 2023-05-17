664674683e5314e2b93b8d8068d9848330253767
Anthony <aaarias6@espe.edu.ec>
2023-05-16 21:23:42 -0500

    Act01

---
+++ js/formregistro.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mi página HTML</title>
+  <script src="script.js"></script>
+</head>
+<body>
+  <!-- Resto del contenido de tu página -->
+  <input type="checkbox" id="Entrega1" onclick="ActivarB()">
+  <button id="Submitb" disabled>Botón</button>
+  <input type="checkbox" id="Entrega2" onclick="ActivarF()">
+  <form id="form1" style="display: none;">
+    <!-- Contenido del formulario -->
+  </form>
+</body>
+</html>
